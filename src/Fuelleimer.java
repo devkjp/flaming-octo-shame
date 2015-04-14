a9096425adb355b9271d09731f15f6a5f0e7298e
Jonas Polkehn <jonas.polkehn@googlemail.com>
2015-04-14 12:08:30 +0200

    renaming of fill algorithm

--- src/Fuelleimer.java
+++ src/Fuelleimer.java
@@ -2,22 +2,22 @@
 public class Fuelleimer {
 	
 	
-	public static void fill(GraphArea.color[][] canvas, int startX, int startY, GraphArea.color code){
+	public static void recSeedFill(GraphArea.color[][] canvas, int startX, int startY, GraphArea.color code){
 		if (startX >= 0 && startX < canvas[0].length) {
 			if (startY >= 0 && startY < canvas.length ){
 				canvas[startX][startY] = code;
 				
 				if (startX - 1 >= 0 && canvas[startX-1][startY] == GraphArea.color.BLANK){
-					fill(canvas, startX-1, startY, code);
+					recSeedFill(canvas, startX-1, startY, code);
 				}
 				if (startX + 1 < canvas[0].length && canvas[startX+1][startY] == GraphArea.color.BLANK){
-					fill(canvas, startX+1, startY, code);
+					recSeedFill(canvas, startX+1, startY, code);
 				}
 				if (startY - 1 >= 0 && canvas[startX][startY-1] == GraphArea.color.BLANK){
-					fill(canvas, startX, startY-1, code);
+					recSeedFill(canvas, startX, startY-1, code);
 				}
 				if (startY + 1 < canvas.length && canvas[startX][startY+1] == GraphArea.color.BLANK){
-					fill(canvas, startX, startY+1, code);
+					recSeedFill(canvas, startX, startY+1, code);
 				}
 			}
 		}
--- src/GUI.java
+++ src/GUI.java
@@ -23,15 +23,15 @@ public class GUI extends JFrame {
 	public static void main(String[] args) {
 		GraphArea gA = new GraphArea(500, 500);
 		Bresenham.drawCircle(gA.getPixelArray(), 25,25,2, GraphArea.color.BLACK);
-		Fuelleimer.fill(gA.getPixelArray(), 25, 25, GraphArea.color.BLACK);
+		Fuelleimer.recSeedFill(gA.getPixelArray(), 25, 25, GraphArea.color.BLACK);
 		Bresenham.drawCircle(gA.getPixelArray(), 25,25,5, GraphArea.color.BLACK);
-		Fuelleimer.fill(gA.getPixelArray(), 25, 22, GraphArea.color.WHITE);
+		Fuelleimer.recSeedFill(gA.getPixelArray(), 25, 22, GraphArea.color.WHITE);
 		Bresenham.drawCircle(gA.getPixelArray(), 25,25,20, GraphArea.color.BLACK);
 		Bresenham.drawLine(gA.getPixelArray(), 5, 25, 21, 25, GraphArea.color.BLACK);
 		Bresenham.drawLine(gA.getPixelArray(), 30, 25, 45, 25, GraphArea.color.BLACK);
-		Fuelleimer.fill(gA.getPixelArray(), 0, 0, GraphArea.color.GRAY);
-		Fuelleimer.fill(gA.getPixelArray(), 6, 24, GraphArea.color.RED);
-		Fuelleimer.fill(gA.getPixelArray(), 6, 26, GraphArea.color.WHITE);
+		Fuelleimer.recSeedFill(gA.getPixelArray(), 0, 0, GraphArea.color.GRAY);
+		Fuelleimer.recSeedFill(gA.getPixelArray(), 6, 24, GraphArea.color.RED);
+		Fuelleimer.recSeedFill(gA.getPixelArray(), 6, 26, GraphArea.color.WHITE);
 		
 		GUI g = new GUI(gA);
 	}
