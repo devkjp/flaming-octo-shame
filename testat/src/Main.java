b0bfe21067cc6d1f2f335faa21ca0d42bf6b216f
Jonas Polkehn <devjp@posteo.de>
2015-06-07 18:53:49 +0200

    Drei Achsenflächen als Basis für weiteres

--- testat/src/Main.java
+++ testat/src/Main.java
@@ -29,22 +29,52 @@ public class Main {
 		int gradV = 2;
 		double[] knotenvektorU = new double[] { 0, 0, 6, 6 };
 		double[] knotenvektorV = new double[] { 0, 0, 6, 6 };
-		Point3d[][] kontrollpunktMatrix = new Point3d[][] {
-				{ new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(2, 0, 0) },
-				{ new Point3d(0, 0, 1), new Point3d(1, 1, 1), new Point3d(2, 0, 1) },
-				{ new Point3d(0, 0, 2), new Point3d(1, 0, 2), new Point3d(2, 0, 2) } };
-		BSplinePatch patch = new BSplinePatch(gradU, gradV, knotenvektorU,
-				knotenvektorV, 3, 3, kontrollpunktMatrix);
 
+		Point3d[][] kontrollpunktMatrix = new Point3d[][] {
+				{ new Point3d(0, 0, 0), new Point3d(1, 1, 0), new Point3d(2, 0, 0) },
+				{ new Point3d(0, 0.5, 1), new Point3d(1, 2, 1),	new Point3d(2, 0.5, 1) },
+				{ new Point3d(0, 0, 2), new Point3d(1, 1, 2), new Point3d(2, 0, 2) } };
+
+		// Flaeche auf Y- Achse
+		Point3d[][] kontrollpunkteFlaecheY = new Point3d[][] {
+				{ new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(2, 0, 0) },
+				{ new Point3d(0, 0, 1), new Point3d(1, 0, 1), new Point3d(2, 0, 1) },
+				{ new Point3d(0, 0, 2), new Point3d(1, 0, 2), new Point3d(2, 0, 2) } };
+
+		BSplinePatch bSplineFlaecheY = new BSplinePatch(gradU, gradV,
+				knotenvektorU, knotenvektorV, 3, 3, kontrollpunkteFlaecheY);
+
+		// Flaeche auf Y- Achse
+		Point3d[][] kontrollpunkteFlaecheX = new Point3d[][] {
+				{ new Point3d(0, 0, 0), new Point3d(0, 1, 0), new Point3d(0, 2, 0) },
+				{ new Point3d(0, 0, 1), new Point3d(0, 1, 1), new Point3d(0, 2, 1) },
+				{ new Point3d(0, 0, 2), new Point3d(0, 1, 2), new Point3d(0, 2, 2) } };
+
+		BSplinePatch bSplineFlaecheX = new BSplinePatch(gradU, gradV, knotenvektorU, knotenvektorV, 3, 3, kontrollpunkteFlaecheX);
+		
+		// Flaeche auf Z- Achse
+		Point3d[][] kontrollpunkteFlaecheZ = new Point3d[][] {
+				{ new Point3d(0, 0, 0), new Point3d(0, 1, 0), new Point3d(0, 2, 0) },
+				{ new Point3d(1, 0, 0), new Point3d(1, 1, 0), new Point3d(1, 2, 0) },
+				{ new Point3d(2, 0, 0), new Point3d(2, 1, 0), new Point3d(2, 2, 0) } };
+
+		BSplinePatch bSplineFlaecheZ = new BSplinePatch(gradU, gradV, knotenvektorU, knotenvektorV, 3, 3, kontrollpunkteFlaecheZ);
+		
+		
+		
 		// Visualisierung definieren
 
 		Group[] g = new Group[] {
-				patch.kontrollPolygon(3, 3, kontrollpunktMatrix),
-				patch.kontrollPunkte(0.05f, 3, 3, kontrollpunktMatrix,
-						new Color3f(0f, 1f, 0f)),
-				patch.segmentierteFlaeche(patch.umin(), patch.vmin(), patch
-						.umax(), patch.vmax(), 20, 20, new Color3f(1f, 1f, 0f)) };
-		
+				bSplineFlaecheX.kontrollPolygon(3, 3, kontrollpunkteFlaecheX),
+				bSplineFlaecheX.kontrollPunkte(0.05f, 3, 3,	kontrollpunkteFlaecheX, new Color3f(0f, 1f, 0f)),bSplineFlaecheX.segmentierteFlaeche(bSplineFlaecheX.umin(), bSplineFlaecheX.vmin(),bSplineFlaecheX.umax(), bSplineFlaecheX.vmax(), 20, 20, new Color3f(1f, 1f,	0f)), 
+				
+				bSplineFlaecheY.kontrollPolygon(3, 3, kontrollpunkteFlaecheY),
+				bSplineFlaecheY.kontrollPunkte(0.05f, 3, 3,	kontrollpunkteFlaecheY, new Color3f(1f, 0f, 0f)),bSplineFlaecheY.segmentierteFlaeche(bSplineFlaecheY.umin(), bSplineFlaecheY.vmin(),bSplineFlaecheY.umax(), bSplineFlaecheY.vmax(), 20, 20, new Color3f(1f, 0f,	1f)),
+
+				bSplineFlaecheZ.kontrollPolygon(3, 3, kontrollpunkteFlaecheZ),
+				bSplineFlaecheZ.kontrollPunkte(0.05f, 3, 3,	kontrollpunkteFlaecheZ, new Color3f(0f, 0f, 1f)),bSplineFlaecheZ.segmentierteFlaeche(bSplineFlaecheZ.umin(), bSplineFlaecheZ.vmin(),bSplineFlaecheZ.umax(), bSplineFlaecheZ.vmax(), 20, 20, new Color3f(0f, 1f,	1f))
+		};
+
 		// Visualisierung starten
 		Visualisierung3D v = new Visualisierung3D(g);
 		v.setTitle("Testat Computergrafik TIT12 - Gruppe Reutebuch, Schulz, Polkehn");
